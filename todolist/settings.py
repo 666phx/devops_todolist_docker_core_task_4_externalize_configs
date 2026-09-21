d84f1a8a2843d156ba0066bb2c3e86c5540b56eb
Oleksandr Kryvulia <oleksandr.kryvulia@gmail.com>
2026-09-21 20:27:57 +0200

    Fixed typos

--- todolist/settings.py
+++ todolist/settings.py
@@ -62,12 +62,12 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': os.environ('ENGINE'),
-        'NAME': os.environ('NAME'),
-        'USER': os.environ('USER'),
-        'PASSWORD': os.environ('PASSWORD'),
-        'HOST': os.environ('HOST'),
-        'PORT': os.environ('PORT'),
+        'ENGINE': os.environ.get('ENGINE'),
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': os.environ.get('PORT'),
     }
 }
 
